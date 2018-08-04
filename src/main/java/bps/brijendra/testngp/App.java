de77c258f63c46131e649f85f10541db97009e80
Brijendra Singh <bps.brijendra@gmail.com>
2018-08-04 17:31:09 +0530

    second commit to add project dirs

---
+++ src/main/java/bps/brijendra/testngp/App.java
@@ -0,0 +1,13 @@
+package bps.brijendra.testngp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

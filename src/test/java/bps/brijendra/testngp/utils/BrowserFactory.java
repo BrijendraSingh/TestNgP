3420bc59003454599f90a85c8eaf23643b4b6543
Brijendra Singh <bps.brijendra@gmail.com>
2018-08-11 20:43:53 +0530

    grid test branch is added for selenium grid test

--- src/test/java/bps/brijendra/testngp/utils/BrowserFactory.java
+++ src/test/java/bps/brijendra/testngp/utils/BrowserFactory.java
@@ -1,11 +1,18 @@
 package bps.brijendra.testngp.utils;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BrowserFactory {
 
-	public WebDriver driver;
+	public RemoteWebDriver driver;
 
 	public WebDriver setWebDeriver(String browser) {
 
@@ -20,8 +27,18 @@ public class BrowserFactory {
 		case "IE":
 
 			break;
-		case "NEW":
-
+		case "REMOTE":
+			//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/main/java/bps/brijendra/testngp/drivers/chromedriver.exe");
+			//ChromeOptions options = new ChromeOptions();
+			DesiredCapabilities cap = new DesiredCapabilities().chrome();
+			cap.setPlatform(Platform.WIN10);
+			cap.setBrowserName("chrome");
+			//options.merge(cap);
+			try {
+				driver = new RemoteWebDriver(new URL("http://192.168.49.20:4547/wd/hub"), cap);
+			} catch (MalformedURLException e) {
+				System.out.println("Exception in remotewebdriver: " +e.getMessage());
+			}
 			break;
 		default:
 			break;
